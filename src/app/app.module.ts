58a172639bfbe51782d519e756568ad18172dec5
michaelbuchanan23 <michaelbuchanan23@gmail.com>
2018-07-31 08:13:50 -0400

    backup 7/31/2018

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { NumberReaderComponent } from './number-reader/number-reader.component';
+import { UserListComponent } from './user/user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    NumberReaderComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
